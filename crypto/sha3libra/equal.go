1f6aee910cab7dceac7d51474e92264f190b4613
tianji <github@wutj.info>
2019-07-21 17:44:03 +0800

    remove log outputs

--- crypto/sha3libra/equal.go
+++ crypto/sha3libra/equal.go
@@ -2,15 +2,14 @@ package sha3libra
 
 import (
 	"bytes"
-	"log"
 )
 
 func Equal(h1, h2 HashValue) bool {
 	if len(h1) != HashSize || len(h2) != HashSize {
 		return false
 	}
-	if bytes.Compare(h1, h2) != 0 {
-		log.Printf("mismatch: %x vs %x", h1, h2)
-	}
+	// if bytes.Compare(h1, h2) != 0 {
+	// 	log.Printf("mismatch: %x vs %x", h1, h2)
+	// }
 	return bytes.Compare(h1, h2) == 0
 }
