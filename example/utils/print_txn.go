506d0510486dd668b4baa77e26f6cea1ddab1e83
tianji <github@wutj.info>
2019-08-27 01:15:38 +0800

    [example utils] more outputs

--- example/utils/print_txn.go
+++ example/utils/print_txn.go
@@ -1,6 +1,7 @@
 package utils
 
 import (
+	"encoding/binary"
 	"encoding/hex"
 	"log"
 	"strings"
@@ -17,10 +18,21 @@ func PrintTxn(txn *types.ProvenTransaction) {
 	log.Printf("        Sender account: %v", hex.EncodeToString(rawTxn.SenderAccount))
 	log.Printf("        Sender seq #%v", rawTxn.SequenceNumber)
 	log.Printf("        Program: %v...", hex.EncodeToString(rawTxn.GetProgram().Code[:30]))
-	log.Printf("            (program name: %s)", stdscript.InferProgramName(rawTxn.GetProgram().Code))
-	log.Printf("        Arg 0: %v", hex.EncodeToString(rawTxn.GetProgram().Arguments[0].Data))
-	log.Printf("        Arg 1: %v", hex.EncodeToString(rawTxn.GetProgram().Arguments[1].Data))
-	log.Printf("    Gas used: %v", txn.GetGasUsed())
+	progName := stdscript.InferProgramName(rawTxn.GetProgram().Code)
+	log.Printf("            (program name: %s)", progName)
+	switch progName {
+	case "peer_to_peer_transfer", "mint":
+		log.Printf("        Arg 0 (receiver address): %v", hex.EncodeToString(rawTxn.GetProgram().Arguments[0].Data))
+		log.Printf("        Arg 1 (amount microLibra): %v", binary.LittleEndian.Uint64(rawTxn.GetProgram().Arguments[1].Data))
+	default:
+		for i, d := range rawTxn.GetProgram().Arguments {
+			log.Printf("        Arg %d: %v", i, hex.EncodeToString(d.Data))
+		}
+	}
+	log.Printf("    Max gas amount (gas units): %v", rawTxn.GetMaxGasAmount())
+	log.Printf("    Gas unit price (microLibra/unit): %v", rawTxn.GetGasUnitPrice())
+	log.Printf("    Expiration timestamp: %v", rawTxn.GetExpirationTime())
+	log.Printf("    Gas used (microLibra): %v", txn.GetGasUsed())
 	if txn.GetWithEvents() {
 		log.Printf("    Events: (%d total)", len(txn.GetEvents()))
 		for idx, ev := range txn.GetEvents() {
