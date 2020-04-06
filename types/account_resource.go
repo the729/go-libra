984258e5014b40fac55254ede75c251a53dff250
tianji <github@wutj.info>
2020-04-06 22:56:09 +0800

    [types] remove ProvenAccountResource type

--- types/account_resource.go
+++ types/account_resource.go
@@ -11,15 +11,7 @@ type AccountResource struct {
 	EventGenerator                 uint64
 }
 
-// ProvenAccountResource is the Libra coin resource of an account which is proven
-// to be included in the ledger.
-type ProvenAccountResource struct {
-	proven          bool
-	accountResource *AccountResource
-	addr            AccountAddress
-	ledgerInfo      *ProvenLedgerInfo
-}
-
+// BalanceResource is LBR balance resource
 type BalanceResource struct {
 	Coin uint64
 }
@@ -34,72 +26,4 @@ func (r *AccountResource) Clone() *AccountResource {
 	out.SequenceNumber = r.SequenceNumber
 	out.EventGenerator = r.EventGenerator
 	return out
-}
-
-// GetLedgerInfo returns the ledger info.
-func (pr *ProvenAccountResource) GetLedgerInfo() *ProvenLedgerInfo {
-	if !pr.proven {
-		panic("not valid proven account resource")
-	}
-	return pr.ledgerInfo
-}
-
-// GetSequenceNumber returns sequence number of the account.
-func (pr *ProvenAccountResource) GetSequenceNumber() uint64 {
-	if !pr.proven {
-		panic("not valid proven account resource")
-	}
-	return pr.accountResource.SequenceNumber
-}
-
-// GetEventGenerator returns event generator of the account.
-func (pr *ProvenAccountResource) GetEventGenerator() uint64 {
-	if !pr.proven {
-		panic("not valid proven account resource")
-	}
-	return pr.accountResource.EventGenerator
-}
-
-// GetAuthenticationKey returns a copy of the hash of public key current in use.
-func (pr *ProvenAccountResource) GetAuthenticationKey() []byte {
-	if !pr.proven {
-		panic("not valid proven account resource")
-	}
-	return cloneBytes(pr.accountResource.AuthenticationKey)
-}
-
-// GetSentEvents returns sent events handle.
-func (pr *ProvenAccountResource) GetSentEvents() *EventHandle {
-	if !pr.proven {
-		panic("not valid proven account resource")
-	}
-	c := &EventHandle{}
-	*c = *pr.accountResource.SentEvents
-	return c
-}
-
-// GetReceivedEvents returns received events handle.
-func (pr *ProvenAccountResource) GetReceivedEvents() *EventHandle {
-	if !pr.proven {
-		panic("not valid proven account resource")
-	}
-	c := &EventHandle{}
-	*c = *pr.accountResource.ReceivedEvents
-	return c
-}
-
-// GetDelegatedWithdrawalCapability returns delegated withdrawal capability.
-func (pr *ProvenAccountResource) GetDelegatedWithdrawalCapability() bool {
-	if !pr.proven {
-		panic("not valid proven account resource")
-	}
-	return pr.accountResource.DelegatedWithdrawalCapability
-}
-
-// GetAddress returns a copy of the address to which this resource belongs.
-func (pr *ProvenAccountResource) GetAddress() AccountAddress {
-	if !pr.proven {
-		panic("not valid proven account resource")
-	}
-	return pr.addr
 }
